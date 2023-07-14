f01d7369489a0aedf66da181ddd08fc93826d481
Felipe Reyes <felipe.reyes@canonical.com>
2023-07-14 15:29:47 -0400

    Make libjuju an extra dependency.
    python-libjuju is released in lockstep with juju, hence if zaza uses a
    2.9 controller, it should use libjuju-2.9.x, for a 3.1 controller it
    should use libjuju-3.1 and so on.
    
    This change makes libjuju an extra, which means depending on the juju
    controller version will be used the right extra should be passed at
    install time.
    
    For juju-2.9:
    
        pip install zaza[juju-29]
    
    For juju-3.1:
    
        pip install zaza[juju-31]

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ install_require = [
 
     'hvac<0.7.0',
     'jinja2',
-    'juju<3.2',
     'juju-wait',
     'PyYAML',
     'tenacity>8.2.0',
@@ -46,6 +45,12 @@ install_require = [
 tests_require = [
     'tox >= 2.3.1',
 ]
+
+extras_require={
+    'testing': tests_require,
+    'juju-29': ['juju<3.0'],
+    'juju-31': ['juju>=3.1.0,<3.2.0'],
+}
 
 
 class Tox(TestCommand):
@@ -110,8 +115,6 @@ setup(
     zip_safe=False,
     cmdclass={'test': Tox},
     install_requires=install_require,
-    extras_require={
-        'testing': tests_require,
-    },
     tests_require=tests_require,
+    extras_require=extras_require,
 )
